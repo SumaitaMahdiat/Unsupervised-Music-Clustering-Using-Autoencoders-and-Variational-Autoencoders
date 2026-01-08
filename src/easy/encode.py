0e77d2ec8b6085b12609d67d042437bbefb4c788
Sumaita Mahdiat Mercy <sumaitamahdiatmercy@gmail.com>
2026-01-09 01:46:59 +0600

    Update encode.py

--- src/easy/encode.py
+++ src/easy/encode.py
@@ -3,28 +3,16 @@ import numpy as np
 from dataset import MusicDataset
 from vae import VAE
 
-# ---------------------------
-# CSV and model path
-# ---------------------------
 csv_file = "C:/Users/user/OneDrive/Documents/musicdata/data/features/easy/train.csv"
 model_path = "results/easy/vae_music.pth"
 
-# ---------------------------
-# Dataset and device
-# ---------------------------
 dataset = MusicDataset(csv_file)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# ---------------------------
-# Load trained VAE
-# ---------------------------
 model = VAE(input_dim=dataset.features.shape[1], latent_dim=16).to(device)
 model.load_state_dict(torch.load(model_path))
 model.eval()
 
-# ---------------------------
-# Encode latent features
-# ---------------------------
 latent_features = []
 with torch.no_grad():
     for x in dataset.features:
